a9bc97e05aedc1fd18e38d0a8b17828e3b5e2fcf
ROELANDT Nicolas <nicolas.roelandt@univ-eiffel.fr>
2021-12-14 09:53:32 +0100

    Update france_tracks view with data download url

--- scripts/03_create_views.sql
+++ scripts/03_create_views.sql
@@ -8,6 +8,7 @@ select nt.pk_track, record_utc, time_length , noise_level,tag_name from noisecap
 INNER JOIN noisecapture_track_tag ntt ON nt.pk_track = ntt.pk_track /* Add track tags*/
 INNER JOIN noisecapture_tag ntag ON ntag.pk_tag = ntt.pk_tag /* Add track tags*/
 where time_length < 900 -- filter records longer than 15 minutes
+and time_length > 5 -- filter too shorts records
 --and tag_name in ('animals')    /*only animal sounds*/
 and nt.pk_track not in ( -- filter tracks that are also taggued indoor or test
   select distinct nt.pk_track from noisecapture_track as nt 
@@ -19,12 +20,16 @@ where ntag.tag_name = 'indoor' or ntag.tag_name =  'test'    /*track recorded in
 
 
 /* Spatial filtering 
- * Returns 5319 records (9293 if not area filter)
+ * Returns 5159 records
  * 44s execution time
  * store result in a MATERIALIZED view
  * */ 
- 
- CREATE MATERIALIZED VIEW france_tracks as (
+DROP MATERIALIZED VIEW IF EXISTS france_tracks;
+
+CREATE MATERIALIZED VIEW france_tracks as (
+ select ft.pk_track, track_uuid, record_utc, time_length, noise_level, pleasantness, 
+concat('https://data.noise-planet.org/raw/', substring(user_uuid, 1, 2),'/', substring(user_uuid, 3, 2),'/', substring(user_uuid, 5, 2),'/',user_uuid,'/','track_',track_uuid,'.zip') download, 
+geog from (
   select * from (
     select * from (
       select pk_track, ST_Area(geog) as sqm_area, geog from (
@@ -42,6 +47,8 @@ where ntag.tag_name = 'indoor' or ntag.tag_name =  'test'    /*track recorded in
 where sqm_area < 625 -- filtering by area
  ) as spatial_query 
 where ST_Intersects(geog, (select geog from countries c where c."admin"  = 'France'))
+) as ft join noisecapture_track nt on nt.pk_track = ft.pk_track 
+join  noisecapture_user nu  ON nt.pk_user = nu.pk_user 
 );
 
 -- Create index
@@ -50,5 +57,5 @@ CREATE INDEX ix_france_tracks_geog_gist ON france_tracks USING gist(geog);
 analyze france_tracks;
 
 
--- Refresh view
+-- Refresh view if changes in tracks_of_interest
 REFRESH MATERIALIZED VIEW france_tracks;
