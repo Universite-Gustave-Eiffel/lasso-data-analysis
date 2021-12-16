730a9e4428a90ad6332e7e9b3ef936066b4d6159
ROELANDT Nicolas <nicolas.roelandt@univ-eiffel.fr>
2021-12-16 12:21:38 +0100

    generalize loading script

--- scripts/00_prepare_database.sh
+++ scripts/00_prepare_database.sh
@@ -5,13 +5,13 @@
 
 # Recreate the schema without loading the data
 # This commands need to know the postgres user password
-sudo -u postgres /usr/lib/postgresql/14/bin/pg_restore --schema-only -d noisecapture -W /home/ame/dump-noisecapture-29_08_2017to28_08_2020_pgv10.15_postgisv2.5.backup
+sudo -u postgres /usr/lib/postgresql/14/bin/pg_restore --schema-only -d noisecapture -W ~/dump-noisecapture-29_08_2017to28_08_2020_pgv10.15_postgisv2.5.backup
 
 # DROP Foreign key Constraints to save disk space
-sudo -u postgres psql -U postgres -d noisecapture -a -f "/home/ame/01_drop_foreign_keys.sql"
+sudo -u postgres psql -U postgres -d noisecapture -a -f "$HOME/01_drop_foreign_keys.sql"
 
 # Load data after removing the foreign key constraints
-sudo -u postgres /usr/lib/postgresql/14/bin/pg_restore -j 2 --data-only -d noisecapture -W /home/ame/dump-noisecapture-29_08_2017to28_08_2020_pgv10.15_postgisv2.5.backup
+sudo -u postgres /usr/lib/postgresql/14/bin/pg_restore -j 2 --data-only -d noisecapture -W ~/dump-noisecapture-29_08_2017to28_08_2020_pgv10.15_postgisv2.5.backup
 
 # We will used the OGR Foreign data wrapper to get NaturalEarth data so it needs to be installed
 sudo apt install postgresql-10-ogr-fdw
