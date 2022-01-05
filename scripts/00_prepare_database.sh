ddba58b55eeb4e0589197e49b9e89e7757bbc611
Nicolas Roelandt <roelandtn.pro@gmail.com>
2022-01-05 08:54:22 +0100

    add bash shebang, comment FK drop (only if needed)

--- scripts/00_prepare_database.sh
+++ scripts/00_prepare_database.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ## Prepare noisecapture database
 ## an empty postgis database is required
 ## The following steps were used to save disk space.
@@ -8,7 +9,7 @@
 sudo -u postgres /usr/lib/postgresql/14/bin/pg_restore --schema-only -d noisecapture -W ~/dump-noisecapture-29_08_2017to28_08_2020_pgv10.15_postgisv2.5.backup
 
 # DROP Foreign key Constraints to save disk space
-sudo -u postgres psql -U postgres -d noisecapture -a -f "$HOME/01_drop_foreign_keys.sql"
+#sudo -u postgres psql -U postgres -d noisecapture -a -f "$HOME/01_drop_foreign_keys.sql"
 
 # Load data after removing the foreign key constraints
 sudo -u postgres /usr/lib/postgresql/14/bin/pg_restore -j 2 --data-only -d noisecapture -W ~/dump-noisecapture-29_08_2017to28_08_2020_pgv10.15_postgisv2.5.backup
